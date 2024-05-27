fa89506e53f48a398626c330b42ae33b24cf8eb0
Satyajit2206 <101856890+Satyajit2206@users.noreply.github.com>
2024-05-27 23:56:29 +0530

    Add files via upload

---
+++ app/src/main/java/com/kanha/bookhub/model/Book.kt
@@ -0,0 +1,10 @@
+package com.kanha.bookhub.model
+
+data class Book(
+    val bookId: String,
+    val bookName: String,
+    val authorName: String,
+    val bookRating: String,
+    val bookPrice: String,
+    val bookCover: String
+)
